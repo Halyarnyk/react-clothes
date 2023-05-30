accbafd11d50b1260f7919256f49a1ad54b49a4a
Halyarnyk <galyarnyk102@gmail.com>
2023-05-30 23:02:02 +0300

    commit 2

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -7,7 +7,7 @@ import { mobile } from "../responsive";
 import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { publicRequest } from "../requestMethods";
-import { addProduct } from "../redux/cartRedux";
+import { addProductForPage } from "../redux/cartRedux";
 import { useDispatch } from "react-redux";
 
 const Container = styled.div``;
@@ -148,7 +148,7 @@ const Product = () => {
 
   const handleClick = () => {
     dispatch(
-      addProduct({ ...product, quantity, color, size })
+      addProductForPage({ ...product, quantity, color, size })
     );
   };
   return (
